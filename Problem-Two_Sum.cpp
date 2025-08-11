380b0b0b994f9fd295711d144892d5520afe54ff
junejareedhima <junejareedhima9@gmail.com>
2025-08-11 16:40:00 +0530

    Add files via upload

---
+++ Problem-Two_Sum.cpp
@@ -0,0 +1,16 @@
+// Problem: Two Sum
+// Link: https://leetcode.com/problems/two-sum/
+// Date: 2025-08-11
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {}; // if no solution
+    }
+};
